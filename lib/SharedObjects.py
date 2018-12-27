272b107136e1477aa2d76a1a851139aaf715d365
Joseph T. Obagbemisoye <jtob91@yahoo.com>
2018-12-27 09:07:24 +0100

    FXCM Trader.ipynb

--- lib/SharedObjects.py
+++ lib/SharedObjects.py
@@ -6,7 +6,7 @@ print a.__class__.__name__
  
 class A(object):
 pass
- 
+
 # this assigns the method to the instance a, but not to the class definition
 a = A()
 a.print_classname = MethodType(print_classname, a, A)
@@ -14,9 +14,59 @@ a.print_classname = MethodType(print_classname, a, A)
 # this assigns the method to the class definition
 A.print_classname = MethodType(print_classname, None, A)
 """
+import pandas
+import threading
 
 class SharedObjects:
+    pd = 'm1'
+    size = 300
+    pairs = dict()
+    
     def __init__(self, con):
         self.connection = con
+        #self.connection.connect()
+        self.tradable_pairs = ['EUR/USD', 'USD/JPY', 'GBP/USD', 'USD/CHF', 
+                  'AUD/USD', 'USD/CAD', 'NZD/USD', 'EUR/GBP', 
+                  'USD/ZAR', 'ZAR/JPY', 'Copper']
+        self.discrepancies = {'EUR/USD': 0.57, 'USD/JPY': 55.5, 'GBP/USD': 0.63, 'USD/CHF': 0.49, 
+                  'AUD/USD': 0.35, 'USD/CAD': 0.68, 'NZD/USD': 0.33, 'EUR/GBP': 0.45, 
+                  'USD/ZAR': 7.3, 'ZAR/JPY': 4, 'Copper': 1.3}
         
+    def get_status(self):
+        status = ""
+        if(self.connection.is_connected() == True):
+            status = "You are logged in"
+        else:
+            status =  "You are logged out"
+        return(status)
     
+    def get_subscribed_symbols(self):
+        return(self.connection.get_subscribed_symbols())
+    
+    def historical_data(self, sym, pd, sz):
+        data = self.connection.get_candles(sym, period=pd, number=sz)
+        dif = self.discrepancies.get(sym)
+        prices = list( ( data['askclose'] + data['bidclose'] / 2 ) - dif ) 
+        opens = list( (data['askopen'] + data['bidopen'] / 2) - dif )
+        highs = list( (data['askhigh'] + data['bidhigh'] / 2) - dif )
+        lows = list( (data['asklow'] + data['bidlow'] / 2) - dif )
+        #ticks_no = data['tickqty']
+        dates = data.axes[0].tolist()
+        d = {'Open': opens, 'High': highs, 'Low': lows, 'Close': prices}
+        df = pandas.DataFrame(data = d, index = dates)
+        return(df)
+    
+    def stream_data(self, sym, handler):
+        self.connection.subscribe_market_data(sym, add_callbacks = handler)
+        return(None)
+    
+    def unstream_data(self):
+        try:
+            def unstream():
+                for symbol in self.tradable_pairs:
+                    self.connection.unsubscribe_market_data(symbol)
+            
+            threading.Thread(target=unstream).start()
+        except:
+            print("Error: unable to start thread")
+        return(None)
--- lib/pairs.py
+++ lib/pairs.py
@@ -1,12 +1,36 @@
-
+import pandas
 
 class Pair:
     
-    def __init__(self, symbol, prices=None):
+    def __init__(self, symbol, dataframe):
         self.sym = symbol
-        self.prices = prices
-        return ''
+        self.prices = dataframe
+        
+    def update(self, df):
+        df['Close'] = ( df['Bid'] + df['Ask'] ) / 2
+        df = df.drop(columns=['Bid', 'Ask'])
+        #self.df = df
+        ts0 = self.prices.axes[0].tolist()[len(self.prices.axes[0].tolist())-1]
+        ts1 = df.axes[0].tolist()[len(df.axes[0].tolist())-1]
+        cl = df['Close'][len(df.axes[0].tolist())-1]
+        high = df['High'][len(df.axes[0].tolist())-1]
+        low = df['Low'][len(df.axes[0].tolist())-1]
+        opn =  self.prices['Close'][len(self.prices.axes[0].tolist())-1]
+        d = {'Open': [opn], 'High': [high], 'Low': [low], 'Close': [cl]}
+        dtf = pandas.DataFrame(data = d, index = [ts1])
+        if(ts0.minute == ts1.minute):
+            ts2 = self.prices.axes[0].tolist()[len(self.prices.axes[0].tolist())-2]
+            self.prices = self.prices[:ts2]
+            self.prices = pandas.concat([self.prices, dtf])
+        else:
+            ts = self.prices.axes[0].tolist()[0]
+            self.prices = self.prices.drop(ts)
+            self.prices = pandas.concat([self.prices, dtf])
+        df = pandas.DataFrame()
+        return(None)
     
-    @classmethod
-    def f1(self, x, y):
-        return min(x, x+y)
+    def on_price_update(self, data, dataframe):
+        if(data['Symbol'] == self.sym):
+            self.update(dataframe)
+        else:
+            return(None)
